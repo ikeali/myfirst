b35a74be8c47e882971ed946c546b55e76fa257d
ikeali <ikechukwualisigwe01@gmail.com>
2024-09-04 12:23:46 +0100

    Update settings.py

--- myfirst/settings.py
+++ myfirst/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cs4&72q=b@5p=%4#f3x9^2x4dzu6h6%l8=_a6@!k!^_nfk-1a)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
